143957e5a81fb0b5fd36861c9eb409b8a699d39d
maharajafz03 <maharajafz1999@gmail.com>
2025-01-05 20:45:12 +0530

    [lkj]

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,23 @@
 import express, { Request, Response } from 'express';
 import { Pool } from 'pg';
-import { client } from './dbconfig';
+import { client } from "./dbconfig";
 
 // Initialize Express
 const app = express();
 const PORT = 4000;
 
+//dancing manajha
+
 // Middleware to parse JSON
 app.use(express.json());
 
 // PostgreSQL connection pool setup
-
+app.post('/dance', async (req: Request, res: Response) => {
+  const {name,password} = req.body;
+  try {
+     await client.query()
+  }
+})
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
